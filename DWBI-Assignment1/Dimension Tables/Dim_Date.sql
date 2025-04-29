32e93230515a997a8a48d55d568eef764079baae
Shashini Thenuwara <167331820+shashithenuwara@users.noreply.github.com>
2025-04-29 19:41:07 +0530

    complete assignment1

--- DWBI-Assignment1/Dimension Tables/Dim_Date.sql
+++ DWBI-Assignment1/Dimension Tables/Dim_Date.sql
@@ -1,10 +1,25 @@
-DROP TABLE IF EXISTS Dim_Date;
-
 CREATE TABLE Dim_Date (
     Date_ID INT PRIMARY KEY,
     Full_Date DATE,
     Day INT,
-    Month_Name VARCHAR(10),
+    Month_Name VARCHAR(10),    -- This line defines the column!
     Quarter VARCHAR(10),
     Year INT
 );
+DECLARE @StartDate DATE = '2020-01-01';
+DECLARE @EndDate DATE = '2030-12-31';
+
+WHILE @StartDate <= @EndDate
+BEGIN
+    INSERT INTO Dim_Date (Date_ID, Full_Date, Day, Month_Name, Quarter, Year)
+    VALUES (
+        CONVERT(INT, FORMAT(@StartDate, 'yyyyMMdd')),
+        @StartDate,
+        DAY(@StartDate),
+        DATENAME(MONTH, @StartDate),
+        'Q' + CAST(DATEPART(QUARTER, @StartDate) AS VARCHAR),
+        YEAR(@StartDate)
+    );
+
+    SET @StartDate = DATEADD(DAY, 1, @StartDate);
+END;
