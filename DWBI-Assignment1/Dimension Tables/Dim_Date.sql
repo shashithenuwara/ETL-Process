d5bd9c0d29c6b60469090c0a8e403c5fdb85bdf8
Shashini Thenuwara <167331820+shashithenuwara@users.noreply.github.com>
2025-05-02 22:13:02 +0530

    Visualizing

--- DWBI-Assignment1/Dimension Tables/Dim_Date.sql
+++ DWBI-Assignment1/Dimension Tables/Dim_Date.sql
@@ -7,19 +7,27 @@ CREATE TABLE Dim_Date (
     Year INT
 );
 DECLARE @StartDate DATE = '2020-01-01';
-DECLARE @EndDate DATE = '2030-12-31';
+DECLARE @EndDate DATE = '2025-12-31';
 
 WHILE @StartDate <= @EndDate
 BEGIN
-    INSERT INTO Dim_Date (Date_ID, Full_Date, Day, Month_Name, Quarter, Year)
+    INSERT INTO dbo.Dim_Date (
+        Date_ID,
+        Full_Date,
+        Day,
+        Month_Name,
+        Quarter,
+        Year
+    )
     VALUES (
-        CONVERT(INT, FORMAT(@StartDate, 'yyyyMMdd')),
-        @StartDate,
-        DAY(@StartDate),
-        DATENAME(MONTH, @StartDate),
-        'Q' + CAST(DATEPART(QUARTER, @StartDate) AS VARCHAR),
-        YEAR(@StartDate)
+        CONVERT(INT, FORMAT(@StartDate, 'yyyyMMdd')),  
+        @StartDate,                                     
+        DAY(@StartDate),                             
+        DATENAME(MONTH, @StartDate),                    
+        DATEPART(QUARTER, @StartDate),                  
+        YEAR(@StartDate)                                
     );
 
     SET @StartDate = DATEADD(DAY, 1, @StartDate);
 END;
+
